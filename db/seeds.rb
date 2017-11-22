c43afbc4ef285312908e0c8381262aa9ed4c6ce3
guillaume schwaederle <guillaume.schwaederle@gmail.com>
2017-11-22 12:52:44 +0100

    Add rating to Review

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
+20.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Coffee.blend_name,
+    address: Faker::Address.community,
+    phone_number:Faker::PhoneNumber.phone_number,
+    category: CATEGORY[rand(0..4)],
+    )
+end
